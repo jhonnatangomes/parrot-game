0e55a03ee8e273cce4586a872685c4725abe813c
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-07-27 22:39:53 -0300

    Added sorting of cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,8 @@
 let cardNumber;
+let gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+
+let gif_alts = ["Parrote do Bob Ross", "Parrote Explosivo", "Parrote Festivo", "Parrote Metaleiro", "Parrote Marinheiro", "Parrotes Triplos", "Parrotes Unicórnios"];
+
 
 function gameStart(){
     cardNumber = prompt("Com quantas cartas você deseja jogar? (4-14)");
@@ -16,21 +20,41 @@ function gameStart(){
 }
 
 function generateCards(){
-    const cards = document.querySelectorAll(".card");
+    gifs.sort(comparator);
+    const pickedGifs = gifs.slice(0, cardNumber/2);
+    const cards = pickedGifs.concat(pickedGifs);
+    cards.sort(comparator);
+    
+    displayCards(cards);
+}
+
+function displayCards(cards){
+    const cardsContainer = document.querySelector(".cards-container");
 
     for(let i = 0; i < cardNumber; i++){
-        cards[i].classList.remove("hidden");
+        cardsContainer.innerHTML += `
+        <div class="card" onclick="turnCard(this);">
+            <div class="front-face face">
+                <img src="images/front.png" alt="Carta de papagaio">
+            </div>
+            <div class="back-face face">
+                <img src="images/${cards[i]}">
+            </div>
+        </div>
+        `
     }
+    
 }
 
 function turnCard(element){
-
     const frontFace = element.querySelector(".front-face");
     const backFace = element.querySelector(".back-face");
-
     backFace.style.transform = "rotateY(0deg)";
     frontFace.style.transform = "rotateY(180deg)";
+}
 
+function comparator(){
+    return Math.random() - 0.5;
 }
 
 gameStart();
