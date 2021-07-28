bcc18252df8a030bc8ddaa1ba0e35cd5f2ad912b
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-07-27 23:47:51 -0300

    Added functionality of turning face down wrong pairs and mantaining face up correct ones

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,11 +3,14 @@ let gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metal
 
 let gif_alts = ["Parrote do Bob Ross", "Parrote Explosivo", "Parrote Festivo", "Parrote Metaleiro", "Parrote Marinheiro", "Parrotes Triplos", "Parrotes Unicórnios"];
 
+let pair = 1;
+let currentTurnedCard;
+
 
 function gameStart(){
     cardNumber = prompt("Com quantas cartas você deseja jogar? (4-14)");
 
-    while(cardNumber < 4 || cardNumber > 14 || cardNumber % 2 == 1){
+    while(cardNumber < 4 || cardNumber > 14 || cardNumber % 2 === 1){
         if(cardNumber >= 4 && cardNumber <= 14){
             cardNumber = prompt("Por favor digite um número par. \nCom quantas cartas você deseja jogar? (4-14)");
         }
@@ -51,10 +54,35 @@ function displayCards(cards){
 
 function turnCard(element){
     const frontFace = element.querySelector(".front-face");
-    const backFace = element.querySelector(".back-face");
-    backFace.style.transform = "rotateY(0deg)";
-    frontFace.style.transform = "rotateY(180deg)";
+    const backFace = element.querySelector(".back-face");  
+    
+    if(pair === 1){
+        backFace.style.transform = "rotateY(0deg)";
+        frontFace.style.transform = "rotateY(180deg)";
+        currentTurnedCard = element;
+        pair = 2;
+    }
+    else{
+        backFace.style.transform = "rotateY(0deg)";
+        frontFace.style.transform = "rotateY(180deg)";
+
+        setTimeout(turnCardDown, 1000, element, currentTurnedCard);
+        pair = 1;
+    }
+    
 }
+
+function turnCardDown(element, currentTurnedCard){
+
+    if(element.querySelector(".back-face img").src !== currentTurnedCard.querySelector(".back-face img").src){
+        currentTurnedCard.querySelector(".front-face").style.transform = "rotateY(0deg)";
+        currentTurnedCard.querySelector(".back-face").style.transform = "rotateY(180deg)";
+        element.querySelector(".front-face").style.transform = "rotateY(0deg)";
+        element.querySelector(".back-face").style.transform = "rotateY(180deg)";
+    }
+
+}
+
 
 function comparator(){
     return Math.random() - 0.5;
